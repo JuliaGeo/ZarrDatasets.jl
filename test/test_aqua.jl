ea30ebd2c54b75884ad3736735a58d0997574cd0
Alexander Barth <barth.alexander@gmail.com>
2024-04-30 10:29:00 +0200

    enable other Aqua tests

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -1,4 +1,7 @@
 using Aqua
 using ZarrDatasets
 
+
 Aqua.test_ambiguities(ZarrDatasets)
+# some internal ambiguities in DiskArray 0.3 probably fixed in 0.4
+Aqua.test_all(ZarrDatasets, ambiguities = false)
